630c72cd9ea125983d9d97d6b53ae3c3960d3081
Laurence Summers <laurence@mintedbox.com>
2023-09-17 18:44:11 +0100

    Refines lightbox design

--- src/components/portfolio/PortfolioLightbox.tsx
+++ src/components/portfolio/PortfolioLightbox.tsx
@@ -9,7 +9,6 @@ import {
 import { CSSTransition } from "react-transition-group";
 import { PortfolioLightBoxItemInterface } from "../../types/portfolioTypes.ts";
 import { sortingDateToMonthYear } from "../../utils/formatDates.ts";
-import PortfolioLightboxRow from "./PortfolioLightboxRow.tsx";
 
 const PortfolioLightbox = ({
     item,
@@ -25,7 +24,7 @@ const PortfolioLightbox = ({
     const [imageLoaded, setImageLoaded] = useState(false);
     useEffect(() => {
         const img = new Image();
-        img.src = item?.item?.img;
+        img.src = item?.item?.img || "";
         img.onload = () => setImageLoaded(true);
     }, [item?.item?.img]);
 
@@ -45,7 +44,7 @@ const PortfolioLightbox = ({
         >
             <div
                 ref={nodeRef}
-                className={`portfolio-lightbox position-fixed d-grid grid-columns-2 gap-3 t-0 l-0 w-100 h-100 text-light`}
+                className={`portfolio-lightbox position-fixed d-grid t-0 l-0 w-100 h-100 text-light`}
                 onClick={() => setInProp(false)}
             >
                 <div className={"position-relative"}>
@@ -73,21 +72,18 @@ const PortfolioLightbox = ({
                         "portfolio-lightbox-content-container p-3 ps-none"
                     }
                 >
-                    <PortfolioLightboxRow label={"What?"}>
-                        <h1>{item?.item?.title}</h1>
-                    </PortfolioLightboxRow>
-                    <PortfolioLightboxRow label={"When?"}>
-                        <h6>{sortingDateToMonthYear(item?.item?.date)}</h6>
-                    </PortfolioLightboxRow>
-                    <PortfolioLightboxRow label={"Why?"}>
-                        <p>{item?.item?.description}</p>
-                    </PortfolioLightboxRow>
-                    <PortfolioLightboxRow label={"Who?"}>
-                        <h3>{item?.item?.client}</h3>
-                    </PortfolioLightboxRow>
-                    <PortfolioLightboxRow label={"How?"}>
-                        <p>{item?.item?.stack}</p>
-                    </PortfolioLightboxRow>
+                    <h1
+                        className={
+                            "portfolio-lightbox-content-back-text position-absolute t-0 fw-900"
+                        }
+                    >
+                        {item?.item?.title}
+                    </h1>
+                    <h1>{item?.item?.title}</h1>
+                    <h6>{sortingDateToMonthYear(item?.item?.date)}</h6>
+                    <p>{item?.item?.description}</p>
+                    <h3>{item?.item?.client}</h3>
+                    <p>{item?.item?.stack}</p>
                 </div>
             </div>
         </CSSTransition>
