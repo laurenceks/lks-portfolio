6bcb01ff35289ba7e3deb79a7e95976ab47ba6dd
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2023-12-10 16:11:25 +0000

    Initial change to using transform for lightbox entrance transition for performance (landscape only)

--- src/components/portfolio/PortfolioLightbox.tsx
+++ src/components/portfolio/PortfolioLightbox.tsx
@@ -1,4 +1,10 @@
-import { Ref, useContext, useEffect, useRef, useState } from "react";
+import {
+    useContext,
+    useEffect,
+    useLayoutEffect,
+    useRef,
+    useState,
+} from "react";
 import { CSSTransition } from "react-transition-group";
 import { sortingDateToMonthYear } from "../../utils/formatDates.ts";
 import PortfolioLightboxStack from "./PortfolioLightboxStack.tsx";
@@ -11,13 +17,68 @@ const PortfolioLightbox = () => {
         dispatchAppState,
     } = useContext(AppContext);
 
-    const nodeRef = useRef() as Ref<HTMLDivElement> | undefined;
+    const nodeRef = useRef<HTMLDivElement>(null);
+    const imgContainerRef = useRef<HTMLDivElement>(null);
     const [imageLoaded, setImageLoaded] = useState(false);
+
+    const [imgContainerTransform, setImgContainerTransform] = useState("");
+
     useEffect(() => {
         const img = new Image();
         img.src = currentPortfolioItem?.item?.img || "";
         img.onload = () => setImageLoaded(true);
     }, [currentPortfolioItem?.item?.img]);
+
+    useLayoutEffect(() => {
+        if (
+            imgContainerRef?.current?.getBoundingClientRect &&
+            currentPortfolioItem?.rect
+        ) {
+            const { top, left, width, height } =
+                imgContainerRef.current.getBoundingClientRect();
+            const { paddingTop, paddingBottom, paddingLeft } = getComputedStyle(
+                imgContainerRef?.current
+            );
+            const parsedPaddingLeft = parseInt(paddingLeft, 10);
+            const parsedPaddingTop = parseInt(paddingTop, 10);
+            const parsedPaddingBottom = parseInt(paddingBottom, 10);
+
+            const isLandscape =
+                currentPortfolioItem.rect.width >
+                currentPortfolioItem.rect.height;
+
+            const scaleX =
+                currentPortfolioItem.rect.width /
+                (width - parsedPaddingLeft - left);
+
+            const imgAspectRatio =
+                currentPortfolioItem.rect.height /
+                currentPortfolioItem.rect.width;
+            const containerHeightScale = imgAspectRatio / (height / width);
+            console.log(containerHeightScale);
+            const scaleY =
+                currentPortfolioItem.rect.height /
+                (height * containerHeightScale -
+                    parsedPaddingTop -
+                    parsedPaddingBottom -
+                    top);
+
+            const translateX =
+                currentPortfolioItem.rect.left +
+                currentPortfolioItem.rect.width / 2 -
+                (parsedPaddingLeft * scaleX) / 2;
+
+            const translateY =
+                currentPortfolioItem.rect.top +
+                currentPortfolioItem.rect.height / 2;
+
+            setImgContainerTransform(
+                `translate(calc(${translateX}px - 50%), calc(${translateY}px - 50%)) scale(${
+                    isLandscape ? scaleX : scaleY
+                })    `
+            );
+        }
+    }, [currentPortfolioItem?.rect]);
 
     return (
         <CSSTransition
@@ -42,13 +103,8 @@ const PortfolioLightbox = () => {
             >
                 <div className={"position-relative"}>
                     <div
-                        className={`portfolio-lightbox-image-container img-contain position-absolute p-3 pe-none`}
-                        style={{
-                            width: currentPortfolioItem?.rect?.width,
-                            height: currentPortfolioItem?.rect?.height,
-                            left: currentPortfolioItem?.rect?.left,
-                            top: currentPortfolioItem?.rect?.top,
-                        }}
+                        className={`portfolio-lightbox-image-container img-contain position-absolute p-3 pe-none w-100 h-100`}
+                        style={{ transform: imgContainerTransform }}
                     >
                         <img
                             src={
@@ -59,6 +115,10 @@ const PortfolioLightbox = () => {
                             alt={currentPortfolioItem?.item?.alt}
                         />
                     </div>
+                    <div
+                        className={`position-absolute p-3 pe-none w-100 h-100`}
+                        ref={imgContainerRef}
+                    ></div>
                 </div>
                 <div
                     className={
